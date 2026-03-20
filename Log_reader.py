19f0b5895b16792501dc578a9102e6378cb22246
varun2003raj <162411169+varun2003raj@users.noreply.github.com>
2026-03-20 13:07:36 -0400

    Update Log_reader.py

--- Log_reader.py
+++ Log_reader.py
@@ -1,1 +1,11 @@
 
+import subprocess
+
+def get_log_stream():
+    process = subprocess.Popen(
+        ["journalctl", "-f"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    return process
