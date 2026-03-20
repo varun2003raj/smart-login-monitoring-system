72140c001c203039bb92e3829a03fb2539a29bb9
varun2003raj <162411169+varun2003raj@users.noreply.github.com>
2026-03-20 13:09:06 -0400

    Update Risk_engine.py

--- Risk_engine.py
+++ Risk_engine.py
@@ -1,1 +1,58 @@
 
+known_ips = set()
+
+def check_new_ip(ip):
+    global known_ips
+
+    if ip == "local" or ip == "unknown":
+        return False
+
+    if ip not in known_ips:
+        known_ips.add(ip)
+        return True
+
+    return False
+
+
+def classify_login(ip, failed_count, unusual_time):
+    if ip == "local" or ip == "unknown":
+        if failed_count >= 3 or unusual_time:
+            return "Suspicious"
+        return "Normal"
+
+    if ip not in known_ips:
+        known_ips.add(ip)
+        return "Suspicious"
+
+    if failed_count >= 3:
+        return "Suspicious"
+
+    if unusual_time:
+        return "Suspicious"
+
+    return "Normal"
+
+
+def assign_risk(ip, failed_count, unusual_time, threshold):
+    if failed_count >= threshold:
+        return "High"
+
+    if ip == "local" or ip == "unknown":
+        if failed_count == 0 and not unusual_time:
+            return "Low"
+        elif failed_count < 3 or unusual_time:
+            return "Medium"
+        else:
+            return "High"
+
+    if ip not in known_ips:
+        return "Medium"
+
+    if failed_count == 0 and not unusual_time:
+        return "Low"
+
+    if failed_count < 3 or unusual_time:
+        return "Medium"
+
+    return "High"
+                 
