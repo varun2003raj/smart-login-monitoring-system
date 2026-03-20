9ded69c7a05107fcbbdba9b0308d960f6140d3fb
varun2003raj <162411169+varun2003raj@users.noreply.github.com>
2026-03-20 13:13:23 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,1 +1,16 @@
+import platform
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+BRUTE_FORCE_THRESHOLD = 5
+UNUSUAL_START = 0
+UNUSUAL_END = 5
+
+SYSTEM_OS = platform.system()
+
+if SYSTEM_OS == "Windows":
+    LOG_SOURCE = "windows"
+else:
+    LOG_SOURCE = "linux"
 
